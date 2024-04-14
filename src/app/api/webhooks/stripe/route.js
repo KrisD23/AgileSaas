278715da9db5d4d9101fdb8fe755e7c83ace1365
Krishan Dwivedi <krishandwive9@gmail.com>
2024-04-14 12:43:00 +0530

    stripe seteup completed

---
+++ src/app/api/webhooks/stripe/route.js
@@ -0,0 +1,51 @@
+import { headers } from "next/headers";
+
+import { stripe } from "@/lib/stripe";
+// import { connectToDb } from "@/lib/utils";
+// import { User } from "@/lib/models";
+
+export async function POST(req) {
+  const body = await req.text();
+
+  const signature = headers().get("Stripe-Signature");
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    return new Response("webhook error", { status: 400 });
+  }
+
+  const session = event.data.object;
+
+  if (event.type === "checkout.session.completed") {
+    const userId = session.metadata.userId;
+    try {
+      console.log("userId:", userId);
+      //   await connectToDb();
+      //   const foundUser = await User.findOne({ username: userId });
+      //   if (!foundUser) {
+      //     throw new Error("User not found");
+      //   }
+      //   console.log("isPremiumUser:", foundUser.isPremiumUser);
+      //   if (!foundUser.isPremiumUser) {
+      //     foundUser.isPremiumUser = true;
+      //     await foundUser.save();
+      //     console.log("User upgraded to premium.");
+      //   } else {
+      //     console.log("User is already a premium user.");
+      //   }
+    } catch (error) {
+      console.error("Error getting payment status:", error);
+    }
+
+    console.log(12);
+  }
+
+  return new Response(null, { status: 200 });
+}
