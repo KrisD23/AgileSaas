545531b52c4a1e8471680819830ecb9bfbec5308
Krishan Dwivedi <krishandwive9@gmail.com>
2024-04-14 17:55:13 +0530

    can display all the queries

--- src/app/dashboard/queries/page.jsx
+++ src/app/dashboard/queries/page.jsx
@@ -1,8 +1,7 @@
 import { getQueries } from "@/lib/data";
-import React from "react";
 
 const page = async () => {
-  // const queries = await getQueries();
+  const queries = await getQueries();
   return <div>Querry page</div>;
 };
 
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,9 +1,17 @@
-import { Answer } from "./models";
+import { Answer, User } from "./models";
+import { connectToDb } from "./utils";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const getQueries = async () => {
+  const { getUser } = getKindeServerSession();
+  const userId = (await getUser()).id;
   try {
     await connectToDb();
-    const queries = await Answer.find();
+    const user = await User.findOne({ username: userId });
+    console.log("user:", user);
+    const queries = await Answer.find({ user: user._id });
+    console.log("queries:", queries);
+    // const queries = await Answer.find(username);
     return queries;
   } catch (error) {
     console.log(error);
