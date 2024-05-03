06a1b65b45fa01eb871dbaa6b3babc9415751458
Krishan Dwivedi <krishandwive9@gmail.com>
2024-05-03 14:05:15 +0530

    finishned the project

--- src/app/dashboard/DashboardComponent.jsx
+++ src/app/dashboard/DashboardComponent.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const DashboardComponent = ({ user }) => {
   return (
     <div className="h-screen w-full justify-center items-center flex flex-col gap-2">
-      <h1 className="text-3xl font-bold">Wellcome to Agile Saas</h1>
+      <h1 className="text-3xl font-bold">Welcome to Agile SaaS</h1>
       <h2 className="text-gray-500 font-semibold text-xl">{user.given_name}</h2>
       {/* <p className="text-blue-500 font-semibold uppercase">Go to :</p> */}
       <div className="flex gap-2 ">
